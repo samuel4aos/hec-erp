d271a0ed3d53d33e61f9a15ae547fb06c7616f5c
samuel4aos <samuel4aos@gmail.com>
2026-05-14 22:46:41 +0100

    fix: full Express app on Vercel with CommonJS

--- api/index.js
+++ api/index.js
@@ -1,5 +1,2 @@
-const handler = (req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ status: "ok" }));
-};
-module.exports = handler;
+const app = require('../server/index');
+module.exports = app;
