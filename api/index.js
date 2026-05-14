ab5f15b2d89e98cd95424c3e3b311239a0db224e
samuel4aos <samuel4aos@gmail.com>
2026-05-14 22:33:46 +0100

    debug: minimal Express function for Vercel

--- api/index.js
+++ api/index.js
@@ -1,2 +1,5 @@
-import app from "../server/index.js";
+import express from "express";
+const app = express();
+app.get("/api/health", (req, res) => res.json({ status: "ok" }));
+app.get("*", (req, res) => res.send("Hello from Vercel"));
 export default app;
