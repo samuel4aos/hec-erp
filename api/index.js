b04401fd70e68846491948b8cc93f2dbd219f16c
samuel4aos <samuel4aos@gmail.com>
2026-05-14 22:44:40 +0100

    debug: use CommonJS, minimal handler

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
-export default (req, res) => {
+const handler = (req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
   res.end(JSON.stringify({ status: "ok" }));
 };
+module.exports = handler;
